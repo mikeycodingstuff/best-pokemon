1eb409519c36462b3b7ad6dd55094fe3090f4c92
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-02-22 20:29:57 +0000

    some page styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
+  <div class="p-3">
+  <h1 class="text-center text-7xl">Best Pokémon</h1>
+  <nav class="flex p-3 gap-3 justify-center">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/test">Test</RouterLink>
   </nav>
+  </div>
   <main>
     <RouterView />
   </main>
