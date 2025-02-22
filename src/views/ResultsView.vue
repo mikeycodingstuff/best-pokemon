104f00b5557a5d2bfd8b5b4bfbfb17a89889081e
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-02-22 20:45:20 +0000

    results page

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <h1 class="text-2xl">This will be a results page</h1>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
