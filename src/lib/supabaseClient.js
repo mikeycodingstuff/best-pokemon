05fba4a1daeabdb98f6fd2ab6db7f7ffa5c72873
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-02-22 14:24:20 +0000

    supabase client

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_DATABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+)
