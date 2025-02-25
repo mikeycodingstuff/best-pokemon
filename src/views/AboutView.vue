84708460d5b52d45f8cc999cd9b685d4231a24c7
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-02-25 20:15:48 +0000

    format

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex justify-center items-center min-h-screen">
     <div class="max-w-2xl self-start mt-12">
-    <p class="text-center">This is a very simple Vue app I built to teach myself how to deploy an app to production! It is published to Netlify and uses Supabase for the back end.</p>
+      <p class="text-center">
+        This is a very simple Vue app I built to teach myself how to deploy an app to production! It
+        is published to Netlify and uses Supabase for the back end.
+      </p>
     </div>
   </div>
 </template>
