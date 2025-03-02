c1d065785acdcfb4d5c99a94d961a3ce748b47da
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-03-02 13:39:21 +0000

    about styling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen">
-    <div class="max-w-2xl self-start mt-12">
+  <div class="flex justify-center items-center h-full">
+    <div class="max-w-2xl text-lg self-start mt-12 space-y-3">
       <p class="text-center">
-        This is a very simple Vue app I built to teach myself how to deploy an app to production! It
-        is published to Netlify and uses Supabase for the back end.
+        This is a very simple
+        <a href="https://vuejs.org/" class="text-indigo-300 hover:text-indigo-500">Vue</a> app I
+        built to teach myself how to deploy an app to production!
+      </p>
+      <p class="text-center">
+        It is published to
+        <a href="https://www.netlify.com/" class="text-indigo-300 hover:text-indigo-500">Netlify</a>
+        and uses
+        <a href="https://supabase.com/" class="text-indigo-300 hover:text-indigo-500">Supabase</a>
+        for the back end.
       </p>
     </div>
   </div>
