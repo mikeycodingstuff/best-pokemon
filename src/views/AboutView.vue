69dc8ba9c036374a8ac5d26d810e84bf76dda1b9
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-03-02 14:17:13 +0000

    styling on links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,14 +3,14 @@
     <div class="max-w-2xl text-lg self-start mt-12 space-y-3">
       <p class="text-center">
         This is a very simple
-        <a href="https://vuejs.org/" class="text-indigo-300 hover:text-indigo-500">Vue</a> app I
+        <a href="https://vuejs.org/" class="text-indigo-300 hover:text-indigo-400 transition-colors">Vue</a> app I
         built to teach myself how to deploy an app to production!
       </p>
       <p class="text-center">
         It is published to
-        <a href="https://www.netlify.com/" class="text-indigo-300 hover:text-indigo-500">Netlify</a>
+        <a href="https://www.netlify.com/" class="text-indigo-300 hover:text-indigo-400 transition-colors">Netlify</a>
         and uses
-        <a href="https://supabase.com/" class="text-indigo-300 hover:text-indigo-500">Supabase</a>
+        <a href="https://supabase.com/" class="text-indigo-300 hover:text-indigo-400 transition-colors">Supabase</a>
         for the back end.
       </p>
     </div>
