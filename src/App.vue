ecd9f6c1b4d4b04102e289f6fdbda0b30ed8450e
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2025-04-18 13:35:24 +0100

    change styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="p-3">
-    <RouterLink to="/" class="flex justify-center text-7xl">Best Pokémon</RouterLink>
-  </div>
   <main>
     <RouterView />
   </main>
--- src/views/MaintenanceView.vue
+++ src/views/MaintenanceView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="h-screen w-screen flex flex-col items-center justify-center bg-stone-900 text-white">
+  <div class="h-screen w-screen bg-stone-900 text-white flex flex-col items-center justify-center text-center px-4">
+    <!-- Title -->
+    <RouterLink to="/" class="text-7xl text-indigo-300 mb-12">
+      Best Pokémon
+    </RouterLink>
+
+    <!-- Maintenance message -->
     <h1 class="text-5xl font-bold">🚧 Currently under maintenance 🚧</h1>
     <p class="text-xl mt-4">Making some updates! Please check back later.</p>
   </div>
